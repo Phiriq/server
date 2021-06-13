50d302158bff3dfe3deb4ea6a0993f4ddd52402c
Vinícius Müller <vinigm.nho@gmail.com>
2021-06-13 02:21:15 -0300

    First commit

---
+++ lib/phiriq.ex
@@ -0,0 +1,18 @@
+defmodule Phiriq do
+  @moduledoc """
+  Documentation for `Phiriq`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Phiriq.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phiriq_test.exs
@@ -0,0 +1,8 @@
+defmodule PhiriqTest do
+  use ExUnit.Case
+  doctest Phiriq
+
+  test "greets the world" do
+    assert Phiriq.hello() == :world
+  end
+end
